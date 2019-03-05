f2657257c903d261237be69e4fafd05a04c5f516
Cag <culucenk@mozilla.com>
2019-03-05 14:25:19 +1100

    First prototype of scheduled scan

--- handler.py
+++ handler.py
@@ -3,43 +3,28 @@ import datetime
 import logging
 
 from util.s3_helper import send_to_s3
-
+from lib.target import Target
 from scanners.httpobs_scanner import HTTPObservatoryScanner
 from util.host_picker import Randomizer
+from util.response import Response
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
-
-
 def run(event, context):
+
     # Test out S3 upload capability
     url = 'https://raw.githubusercontent.com/mozilla/http-observatory-dashboard/master/httpobsdashboard/conf/sites.json'
     randomizer = Randomizer(url)
     scanner = HTTPObservatoryScanner()
-    hostname = randomizer.next()
-    scan_result = scanner.scan(hostname)
-    logger.info(scan_result)
-    send_to_s3(hostname, scan_result)
+    destination = Target(randomizer.next())
+    # Need to perform target validation here
+
+    if not destination.isValid():
+        logger.error("Target Validation Failed of: " +
+                     json.dumps(destination.targetname))
+    else:
+        scan_result = scanner.scan(destination)
+        logger.info(scan_result)
+        send_to_s3(destination, scan_result)
