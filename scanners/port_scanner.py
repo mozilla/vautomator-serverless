cba6ab25bdabd64f6881d4bad1b0ee5a487fc273
Cag <culucenk@mozilla.com>
2019-03-12 16:14:03 +1100

    Clean up
    and make things more readable.

--- scanners/port_scanner.py
+++ scanners/port_scanner.py
@@ -7,14 +7,17 @@ class PortScanner():
 
     def __init__(self):
         # This is needed for nmap static library to be added to the path
-        os.environ['PATH'] = os.environ['PATH'] + ':' + os.environ['LAMBDA_TASK_ROOT'] + '/bin/nmap-standalone/'
-        return None
+        os.environ['PATH'] = os.environ['PATH']
+        + ':' + os.environ['LAMBDA_TASK_ROOT']
+        + '/bin/nmap-standalone/'
     
     def scanTCP(self, host):
         # TODO: There is an async scan option in python-nmap, use that instead
         nm = nmap.PortScanner()
         isSudo = False
-        nmap_arguments = "-v -Pn -sT -sV --script=banner --top-ports 1000 --open -T4 --system-dns"
+        nmap_arguments = ("-v -Pn -sT -sV --script=banner " +
+                          "--top-ports 1000 --open -T4 --system-dns"
+                          )
         results = nm.scan(host, arguments=nmap_arguments, sudo=isSudo)
         time.sleep(120)
 
