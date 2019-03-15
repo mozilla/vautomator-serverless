3e42d834b1a4701a0d36b253a4d3c465f0ff8995
Jonathan Claudius <jclaudius@mozilla.com>
2019-03-15 14:40:48 -0400

    Fix broken master unit-tests

---
+++ tests/test_hosts.py
@@ -0,0 +1,48 @@
+import sys
+import pytest
+import os
+import json
+from util.hosts import Hosts
+
+
+class TestHosts():
+    def test_with_no_hosts(self):
+        hosts = Hosts()
+        assert hosts.next() == None
+
+    def test_with_hosts(self):
+        hostname_array = [
+            "foo.example.com",
+            "bar.example.com",
+            "baz.example.com"
+        ]
+        hosts = Hosts(hostname_array)
+        assert type(hosts.next()) is str
+        assert type(hosts.next()) is str
+        assert type(hosts.next()) is str
+        assert type(hosts.next()) is str
+        assert type(hosts.next()) is str
+
+    def test_with_hosts_for_host_fixing(self):
+        hostname_array = [
+            "foo.example.com",
+            "bar.example.com",
+            "baz.example.com",
+            "foo2.example.com",
+            "bar2.example.com",
+            "baz2.example.com"
+        ]
+        hosts = Hosts(hostname_array)
+
+        # Make 6 selections from next()
+        selected_hosts = []
+        selected_hosts.append(hosts.next())
+        selected_hosts.append(hosts.next())
+        selected_hosts.append(hosts.next())
+        selected_hosts.append(hosts.next())
+        selected_hosts.append(hosts.next())
+        selected_hosts.append(hosts.next())
+
+        # check the len of a uniq'd selection to
+        # make sure we're not stuck on the same host
+        assert len(list(set(selected_hosts))) > 1
