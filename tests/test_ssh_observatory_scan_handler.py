7c8c6b2b9e7a8803d2c3403c0934161d6ca734ef
Cag <culucenk@mozilla.com>
2019-04-01 22:27:28 +1100

    Adding sshscan handler as it was missing

---
+++ tests/test_ssh_observatory_scan_handler.py
@@ -0,0 +1,71 @@
+import pytest
+import boto3
+import time
+
+from lib.sshscan_handler import SSHScanHandler
+from lib.hosts import Hosts
+from moto import mock_sqs
+from uuid import UUID
+
+
+class TestSSHScanHandler():
+    @pytest.fixture
+    def sqs(self, scope="session", autouse=True):
+        mock = mock_sqs()
+        mock.start()
+        # There is currently a bug on moto, this line is needed as a workaround
+        # Ref: https://github.com/spulec/moto/issues/1926
+        boto3.setup_default_session()
+
+        sqs_client = boto3.client('sqs', 'us-west-2')
+        queue_name = "test-scan-queue"
+        queue_url = sqs_client.create_queue(
+            QueueName=queue_name
+        )['QueueUrl']
+
+        yield (sqs_client, queue_url)
+        mock.stop()
+
+    def test_creation(self):
+        sshscan_handler = SSHScanHandler()
+        assert type(sshscan_handler) is SSHScanHandler
+
+    def test_queue(self, sqs):
+        client, queue_url = sqs
+        test_event = {"body": '{"target": "ssh.mozilla.com"}'}
+        test_context = None
+        sshscan_handler = SSHScanHandler(client, queue_url)
+        sshscan_handler.queue(test_event, test_context)
+        response = client.receive_message(QueueUrl=queue_url)
+        scan_type, target, uuid = response['Messages'][0]['Body'].split('|')
+        assert scan_type == "sshobservatory"
+        assert target == "ssh.mozilla.com"
+        assert UUID(uuid, version=4)
+
+    def test_queue_scheduled(self, sqs):
+        client, queue_url = sqs
+        test_hostlist = [
+            "ssh.mozilla.com",
+            "infosec.mozilla.org",
+            "mozilla.org"
+        ]
+        index = 0
+        test_event = None
+        test_context = None
+        sshscan_handler = SSHScanHandler(client, queue_url)
+        sshscan_handler.queue_scheduled(test_event,
+                                        test_context,
+                                        test_hostlist)
+
+        # Need to sleep here for at least 6 seconds as queue messages
+        # have 2 seconds delay until become available
+        time.sleep(7)
+        response = client.receive_message(QueueUrl=queue_url,
+                                          MaxNumberOfMessages=3)
+        for msg in response['Messages']:
+            scan_type, target, placeholder = msg['Body'].split('|')
+            assert scan_type == "sshobservatory"
+            assert target == test_hostlist[index]
+            assert placeholder == ""
+            index += 1
+            
