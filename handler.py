160d45f6021a4027e3a78c300b0fd4fdc5bff85d
Cag <culucenk@mozilla.com>
2019-04-03 11:00:42 +1100

    Use single handler file, 1st attempt

--- handler.py
+++ handler.py
@@ -4,6 +4,10 @@ import os
 
 from lib.s3_helper import send_to_s3
 from lib.target import Target
+from lib.portscan_handler import PortScanHandler
+from lib.httpobsscan_handler import HTTPObsScanHandler
+from lib.tlsobsscan_handler import TLSObsScanHandler
+from lib.sshscan_handler import SSHScanHandler
 from scanners.http_observatory_scanner import HTTPObservatoryScanner
 from scanners.ssh_observatory_scanner import SSHObservatoryScanner
 from scanners.tls_observatory_scanner import TLSObservatoryScanner
@@ -15,6 +19,52 @@ logger.setLevel(logging.INFO)
 SQS_CLIENT = boto3.client('sqs')
 
 
+def queue_portscan(event, context):
+    port_scan_handler = PortScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    response = port_scan_handler.queue(event, context)
+    return response
+
+
+def queue_scheduled_portscan(event, context):
+    scheduled_portscan_handler = PortScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    scheduled_portscan_handler.queue_scheduled(event, context)
+
+
+def queue_httpboservatory(event, context):
+    httpobservatory_handler = HTTPObsScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    response = httpobservatory_handler.queue(event, context)
+    return response
+
+
+def queue_scheduled_httpobservatory(event, context):
+    scheduled_httpobservatory_handler = HTTPObsScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    scheduled_httpobservatory_handler.queue_scheduled(event, context)
+
+
+def queue_tlsobservatory(event, context):
+    tlsobservatory_handler = TLSObsScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    response = tlsobservatory_handler.queue(event, context)
+    return response
+
+
+def queue_scheduled_tlsobservatory(event, context):
+    scheduled_tlsobservatory_handler = TLSObsScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    scheduled_tlsobservatory_handler.queue_scheduled(event, context)
+
+
+def queue_sshobservatory(event, context):
+    sshobservatory_handler = SSHScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    response = sshobservatory_handler.queue(event, context)
+    return response
+
+
+def queue_scheduled_sshobservatory(event, context):
+    scheduled_sshobservatory_handler = SSHScanHandler(sqs_client=SQS_CLIENT, logger=logger)
+    scheduled_sshobservatory_handler.queue_scheduled(event, context)
+
+
+# To leave handler as lean as possible, we should
+# probably move this to another file/module also
 def runScanFromQ(event, context):
 
     # This is needed for nmap static library to be added to the path
