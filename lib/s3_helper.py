7bb32ecbc84af519c9fe7e38881faa9f476fd490
Cag <culucenk@mozilla.com>
2019-04-09 17:39:07 +1000

    Make json results in S3 more readable

--- lib/s3_helper.py
+++ lib/s3_helper.py
@@ -9,7 +9,7 @@ def send_to_s3(hostname, scan_json):
     key = "{}.{}".format(hostname, "json")
     bucket = "vautomator-results"
 
-    client.put_object(Body=json.dumps(scan_json), Bucket=bucket,
+    client.put_object(Body=json.dumps(scan_json, indent=4, sort_keys=True), Bucket=bucket,
                       Key=key, ACL='public-read')
     url = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
     logging.info("Uploaded result file to URL: {}".format(url))
