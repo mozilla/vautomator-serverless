723575c44754c7bed840f4febe886b28aea2ea7f
Cag <culucenk@mozilla.com>
2019-04-09 23:46:08 +1000

    Working ondemand_tasker

--- examples/ondemand_tasker.py
+++ examples/ondemand_tasker.py
@@ -1,23 +1,25 @@
 import logging
-import sys
 import requests
 import json
 import time
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from lib.target import Target
 
 # This is an example of an on demand scan that an engineer might run should they have
 # interest in getting immediate vulnerability data about a given FQDN. This would be
 # the interface an engineer could use to kick off a VA.
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-API_GW_URL = "https://y2ippncfd1.execute-api.us-west-2.amazonaws.com"
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+# API_GW_URL = "https://y2ippncfd1.execute-api.us-west-2.amazonaws.com"
+API_GW_URL = os.environ['API_GW_URL']
 
 fqdn = input("Provide the FQDN (Fully Qualified Domain Name) you want to scan: ")
-# TODO: add relevant tasking call here to /ondemand/portscan
-target = Target(fqdn)
-if not target:
-    logger.error("Target validation failed: target must be an FQDN or IPv4 only.")
+try:
+    target = Target(fqdn)
+except AssertionError:
+    logging.error("Target validation failed: target must be an FQDN or IPv4 only.")
     sys.exit(-1)
 
 portscan_url = API_GW_URL + "/dev/ondemand/portscan"
@@ -26,32 +28,29 @@ tlsobs_scan_url = API_GW_URL + "/dev/ondemand/tlsobservatory"
 sshobs_scan_url = API_GW_URL + "/dev/ondemand/sshobservatory"
 
 session = requests.Session()
-logger.info("Sending POST to https://<YOUR-API-ENDPOINT>/dev/ondemand/portscan")
-response = session.post(portscan_url, data='{"target":"{}"}'.format(target.name).json())
+logging.info("Sending POST to {}".format(portscan_url))
+response = session.post(portscan_url, data="{\"target\":\"" + target.name + "\"}")
 if response.status_code == 200 and 'uuid' in response.json():
-    logger.info("Triggered a Port Scan of: {}".format(target.name))
+    logging.info("Triggered a Port Scan of: {}".format(target.name))
     session.close()
 time.sleep(1)
 
-# TODO: add relevant tasking call here to /ondemand/httpobservatory
-logger.info("Sending POST to https://<YOUR-API-ENDPOINT>/dev/ondemand/httpobservatory")
-response = session.post(httpobs_scan_url, data='{"target":"{}"}'.format(target.name).json())
+logging.info("Sending POST to {}".format(httpobs_scan_url))
+response = session.post(httpobs_scan_url, data="{\"target\":\"" + target.name + "\"}")
 if response.status_code == 200 and 'uuid' in response.json():
-    logger.info("Triggered an HTTP Observatory Scan of: {}".format(target.name))
+    logging.info("Triggered an HTTP Observatory Scan of: {}".format(target.name))
     session.close()
 time.sleep(1)
 
-# TODO: add relevant tasking call here to /ondemand/tlsobservatory
-logger.info("Sending POST to https://<YOUR-API-ENDPOINT>/dev/ondemand/tlsobservatory")
-response = session.post(tlsobs_scan_url, data='{"target":"{}"}'.format(target.name).json())
+logging.info("Sending POST to {}".format(tlsobs_scan_url))
+response = session.post(tlsobs_scan_url, data="{\"target\":\"" + target.name + "\"}")
 if response.status_code == 200 and 'uuid' in response.json():
-    logger.info("Triggered a TLS Observatory Scan of: {}".format(target.name))
+    logging.info("Triggered a TLS Observatory Scan of: {}".format(target.name))
     session.close()
 time.sleep(1)
 
-# TODO: add relevant tasking call here to /ondemand/sshobservatory
-logger.info("Sends POST to https://<YOUR-API-ENDPOINT>/dev/ondemand/sshobservatory")
-response = session.post(sshobs_scan_url, data='{"target":"{}"}'.format(target.name).json())
+logging.info("Sending POST to {}".format(sshobs_scan_url))
+response = session.post(sshobs_scan_url, data="{\"target\":\"" + target.name + "\"}")
 if response.status_code == 200 and 'uuid' in response.json():
-    logger.info("Triggered an SSH Observatory Scan of: {}".format(target.name))
+    logging.info("Triggered an SSH Observatory Scan of: {}".format(target.name))
     session.close()
