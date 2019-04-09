b7c144cd8d6fd1071a3b8f05c159b078305104f3
Cag <culucenk@mozilla.com>
2019-04-09 18:26:06 +1000

    Make handlers expect JSON formatted data only

--- lib/sshscan_handler.py
+++ lib/sshscan_handler.py
@@ -17,35 +17,42 @@ class SSHScanHandler(object):
         self.region = region
 
     def queue(self, event, context):
-        data = json.loads(event['body'])
-        if "target" not in data:
+        try:
+            data = json.loads(event['body'])
+            if "target" not in data:
+                self.logger.error("Unrecognized payload")
+                return Response({
+                    "statusCode": 500,
+                    "body": json.dumps({'error': 'Unrecognized payload'})
+                }).with_security_headers()
+
+            target = Target(data.get('target'))
+            if not target:
+                self.logger.error("Target validation failed of: {}".format(target.name))
+                return Response({
+                    "statusCode": 400,
+                    "body": json.dumps({'error': 'Target was not valid or missing'})
+                }).with_security_headers()
+
+            scan_uuid = str(uuid.uuid4())
+
+            self.sqs_client.send_message(
+                QueueUrl=self.queueURL,
+                MessageBody="sshobservatory|" + target.name
+                + "|" + scan_uuid
+            )
+
+            # Use a UUID for the scan type and return it
+            return Response({
+                "statusCode": 200,
+                "body": json.dumps({'uuid': scan_uuid})
+            }).with_security_headers()
+        except ValueError:
             self.logger.error("Unrecognized payload")
             return Response({
                 "statusCode": 500,
                 "body": json.dumps({'error': 'Unrecognized payload'})
             }).with_security_headers()
-
-        target = Target(data.get('target'))
-        if not target:
-            self.logger.error("Target validation failed of: {}".format(target.name))
-            return Response({
-                "statusCode": 400,
-                "body": json.dumps({'error': 'Target was not valid or missing'})
-            }).with_security_headers()
-
-        scan_uuid = str(uuid.uuid4())
-
-        self.sqs_client.send_message(
-            QueueUrl=self.queueURL,
-            MessageBody="sshobservatory|" + target.name
-            + "|" + scan_uuid
-        )
-
-        # Use a UUID for the scan type and return it
-        return Response({
-            "statusCode": 200,
-            "body": json.dumps({'uuid': scan_uuid})
-        }).with_security_headers()
 
     def queue_scheduled(self, event, context, hostname_list=[]):
         if len(hostname_list) == 0:
