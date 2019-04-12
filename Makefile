5869aae128ccfdd8941f646a11b619929874c183
Cag <culucenk@mozilla.com>
2019-04-12 17:32:15 +1000

    Tinker with Makefile for KMS key creation

--- Makefile
+++ Makefile
@@ -1,21 +1,45 @@
 ROOT_DIR	:= $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 AWS_REGION	:= us-west-2
 AWS_PROFILE := 
+TENABLE_IO := Y
+KMS_POLICY_FILE := 
+KMS_DESCRIPTION := "KMS key for vautomator-serverless"
+KMS_KEY_ALIAS := vautomator
+ifeq ($(TENABLE_IO), Y)
+	KMS_KEYID := $(shell aws --profile '$(AWS_PROFILE)' kms create-key --policy \
+	file://./'$(KMS_POLICY_FILE)' --description '$(KMS_DESCRIPTION)' --query 'KeyMetadata.KeyId')
+endif
+ifdef KMS_KEYID
+	$(shell aws --profile '$(AWS_PROFILE)' kms create-alias --alias-name alias/'$(KMS_KEY_ALIAS)' \
+	--target-key-id '$(KMS_KEYID)'); 
+	# Temporary
+    TENABLE_ACCESS_KEY := XXX; 
+    TENABLE_SECRET_KEY := XXX;
+endif
 
 all:
 	@echo 'Available make targets:'
 	@grep '^[^#[:space:]^\.PHONY.*].*:' Makefile
 
+.PHONY: setup
+setup: export AWS_SDK_LOAD_CONFIG=true
+setup:
+ifeq ($(TENABLE_IO), Y)
+	aws --profile $(AWS_PROFILE) ssm put-parameter --name "TENABLEIO_ACCESS_KEY" \
+	--value $(TENABLE_ACCESS_KEY) --type SecureString --key-id $(KMS_KEYID) && \
+	aws --profile $(AWS_PROFILE) ssm put-parameter --name "TENABLEIO_SECRET_KEY" \
+	--value $(TENABLE_SECRET_KEY) --type SecureString --key-id $(KMS_KEYID) 
+endif
+	npm install serverless-python-requirements --save-dev 
+	
 .PHONY: validate
 validate: export AWS_SDK_LOAD_CONFIG=true
 validate:
-	npm install serverless-python-requirements --save-dev && \
 	sls deploy --noDeploy --region $(AWS_REGION) --aws-profile $(AWS_PROFILE)
 
 .PHONY: deploy
 deploy: export AWS_SDK_LOAD_CONFIG=true
 deploy:
-	npm install serverless-python-requirements --save-dev && \
 	sls deploy --region $(AWS_REGION) --aws-profile $(AWS_PROFILE)
 
 .PHONY: test
--- scanners/tenable_io_scanner.py
+++ scanners/tenable_io_scanner.py
@@ -18,12 +18,16 @@ class TIOScanner():
         self.logger = logger
     
     def scan(self, hostname):
-        # First, check to see if we can read the API keys
-        if self.tio_access_key == "" or self.tio_secret_key == "":
+        # First, 
+        try:
+            # Check to see if we can load the API keys
+            self.tio_access_key, self.tio_secret_key = self.__getAPIKey()
+        except Exception:
             raise Exception(
                 "[!] Cannot obtain Tenable.io API key(s), skipping Tenable.io scan."
             )
             return False
+
         # We can obtain the keys
         self.client = TenableIOClient(access_key=self.tio_access_key, secret_key=self.tio_secret_key)
         # Reference: https://github.com/tenable/Tenable.io-SDK-for-Python/blob/master/examples/scans.py
@@ -38,53 +42,6 @@ class TIOScanner():
         except TenableIOApiException as TIOException:
             self.logger.error("[-] Tenable.io scan failed: {}".format(TIOException))
 
-    def __downloadReport(self, nscan, reportformat="html", style="assets"):
-        report_path = ("/app/results/" + (self.tasktarget).targetdomain
-                       + "/Scan_for_" + (self.tasktarget).targetdomain
-                       + "." + reportformat)
+    def __getAPIKey(self):
 
-        if reportformat == "html":
-            fmt = ScanExportRequest.FORMAT_HTML
-        elif reportformat == "pdf":
-            fmt = ScanExportRequest.FORMAT_PDF
-        elif reportformat == "csv":
-            fmt = ScanExportRequest.FORMAT_CSV
-        elif reportformat == "nessus":
-            fmt = ScanExportRequest.FORMAT_NESSUS
-        elif reportformat == "db":
-            fmt = ScanExportRequest.FORMAT_DB
-        else:
-            return False
-
-        if style == "assets":
-            reportoutline = ScanExportRequest.CHAPTER_CUSTOM_VULN_BY_HOST
-        elif style == "exec":
-            reportoutline = ScanExportRequest.CHAPTER_EXECUTIVE_SUMMARY
-        elif style == "plugins":
-            reportoutline = ScanExportRequest.CHAPTER_CUSTOM_VULN_BY_PLUGIN
-        else:
-            return False
-
-        nscan.download(report_path, format=fmt, chapter=reportoutline)
-
-    def __checkScanStatus(self, nscan):
-        # Query Tenable API to check if the scan is finished
-        status = nscan.status()
-
-        if status == Scan.STATUS_COMPLETED:
-            return "COMPLETE"
-        elif status == Scan.STATUS_ABORTED:
-            return "ABORTED"
-        elif status == Scan.STATUS_INITIALIZING:
-            return "INITIALIZING"
-        elif status == Scan.STATUS_PENDING:
-            return "PENDING"
-        elif status == Scan.STATUS_RUNNING:
-            return "RUNNING"
-        else:
-            self.logger.error("[-] Something is wrong with Tenable.io scan. Check the TIO console manually.")
-            return False
-
-    def __getAPIKey(self, key_type):
-
-        return
+        return a_key, s_key
