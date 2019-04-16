ea9bc48a1d888167edb69dab1ad667177868daf9
Cag <culucenk@mozilla.com>
2019-04-16 18:12:26 +1000

    Shoring up the scanner object and its tests

--- tests/test_tenable_io_scanner.py
+++ tests/test_tenable_io_scanner.py
@@ -3,21 +3,93 @@ import pytest
 import os
 import json
 import requests
+import boto3
 from scanners.tenable_io_scanner import TIOScanner
 from lib.target import Target
 from tenable_io.client import TenableIOClient
-from tenable_io.exceptions import TenableIOApiException
+from tenable_io.exceptions import TenableIOApiException, TenableIOErrorCode
 from tenable_io.api.scans import ScanExportRequest
 from tenable_io.api.models import Scan
+from moto import mock_ssm
+from uuid import UUID
 
 
 class TestTIOScanner():
-    def test_defaults(self):
-        scanner = TIOScanner()
+    # Approach taken here is to create a dedicated Tenable.io 
+    # account just for test purposes, and use Travis CI's 
+    # "secure" method to store the credentials, use those 
+    # credentials to test the "scan" method.
+
+    # Mocking SSM here so we can tes the __getAPIKey() method
+    @pytest.fixture
+    def ssm(self, scope="session", autouse=True):
+        mock = mock_ssm()
+        mock.start()
+        # There is currently a bug on moto, this line is needed as a workaround
+        # Ref: https://github.com/spulec/moto/issues/1926
+        boto3.setup_default_session()
+
+        ssm_client = boto3.client('ssm', 'us-west-2')
+        ssm_client.put_parameter(
+            Name='TENABLEIO_ACCESS_KEY',
+            Description='Bogus access key.',
+            Value='TEST',
+            Type='SecureString'
+        )
+
+        ssm_client.put_parameter(
+            Name='TENABLEIO_SECRET_KEY',
+            Description='Bogus secret key.',
+            Value='TEST',
+            Type='SecureString'
+        )
+        
+        yield (ssm_client)
+        mock.stop()
+
+    def test_defaults(self, ssm):
+        scanner = TIOScanner(ssm_client=ssm)
         assert scanner.client is None
         assert scanner.tio_access_key is None
         assert scanner.tio_secret_key is None
         assert scanner.report_format == "html"
+        assert scanner.ssm_client == ssm
+
+    def test_tenable_auth_fail(self):
+        try:
+            TenableIOClient('test', 'test').session_api.get()
+            assert False
+        except TenableIOApiException as e:
+            assert e.code is TenableIOErrorCode.UNAUTHORIZED
+
+    def test_tenable_auth_success(self):
+        # See if the keys are available as env variables
+        try:
+            a_key = os.environ["TIOA"]
+            s_key = os.environ["TIOS"]
+        except Exception:
+            assert False
+        # See if we can use those keys to successfully
+        # authenticate to Tenable.io, should be True
+        try:
+            TenableIOClient(a_key, s_key).session_api.get()
+            assert True
+        except TenableIOApiException as e:
+            assert e.code is TenableIOErrorCode.UNAUTHORIZED
+
+    def test__getAPIKey(self, ssm):
+        scanner = TIOScanner(ssm_client=ssm)
+        test_akey, test_skey = scanner._TIOScanner__getAPIKey()
+        assert test_akey == "TEST"
+        assert test_skey == "TEST"
 
     def test_scan(self):
-        
+        # See if the keys are available as env variables
+        try:
+            a_key = os.environ["TIOA"]
+            s_key = os.environ["TIOS"]
+        except Exception:
+            assert False
+        host_name = "www.mozilla.org"
+        scanner = TIOScanner(access_key=a_key, secret_key=s_key)
+        nscan = scanner.scan(host_name)
