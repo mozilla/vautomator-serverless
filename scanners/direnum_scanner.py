5f1d6253e99de431888ebdba17ff4e8fb2bc7cff
Cag <culucenk@mozilla.com>
2019-05-06 15:32:24 +1000

    Flesh out the direnum scanner and its tests

--- scanners/direnum_scanner.py
+++ scanners/direnum_scanner.py
@@ -6,33 +6,45 @@ from lib.utilities import sanitise_shell_cmd, wait_process_timeout
 
 class DirectoryEnumScanner():
 
-    def __init__(self, tool="dirb", arguments="-f -w -S -r", logger=logging.getLogger(__name__)):
+    def __init__(self, tool='dirb', arguments="-f -w -S -r", wordlist='short', logger=logging.getLogger(__name__)):
         self.tool = tool
         self.logger = logger
         self.arguments = "".join(sanitise_shell_cmd(arguments))
+        self.wordlist = wordlist
 
     def scan(self, hostname):
+        # Decide on the wordlist first
+        # TODO: Fill these with actual wordlists
+        wordlist_options = {
+            'short': "common.txt",
+            'medium': "common.txt",
+            'long': "common.txt"
+        }
         if self.tool == "dirb":
             # Assume here that standalone dirb binary is in the PATH
             # This is done in the main handler file
             self.logger.info("[+] Running dirb scan on {}".format(hostname))
+
+            results = {}
+            direnum_results = []
+            results['routes'] = direnum_results
+            results['host'] = hostname
             
-            # TODO: Change the wordlist path
             p = run(
-                ["dirb", hostname, "/app/vendor/dirb222/wordlists/common.txt", self.arguments],
+                ["dirb", hostname, wordlist_options[self.wordlist], self.arguments],
                 check=True,
                 stdout=PIPE
             )
-            scan_result = p.stdout
+            direnum_results = p.stdout
             try:
                 # Even though a lambda function can only run for 15 mins max
-                # We should probably kill a scan after 10 mins to be safe
-                wait_process_timeout(p, 600)
+                # We should probably kill a scan after approx. 12 mins to be safe
+                wait_process_timeout(p, 720)
             except RuntimeError:
                 p.kill()
                 self.logger.warning("[!] Directory enum timed out, process killed")
                 return p.returncode, False
             else:
-                return p.returncode, scan_result
+                return p.returncode, results
         else:
             self.logger.error("[-] Unrecognized/unsupported tool for scan.")
---
+++ tests/test_direnum_scanner.py
@@ -0,0 +1,53 @@
+import pytest
+import os
+from scanners.direnum_scanner import DirectoryEnumScanner
+
+
+class TestDirectoryEnumScanner():
+
+    def test_defaults(self):
+        scanner = DirectoryEnumScanner()
+        assert scanner.tool == 'dirb'
+        assert scanner.arguments == '-f -w -S -r'
+        assert scanner.wordlist == 'short'
+
+    # This will never succeed in Travis, because
+    # it relies on dirb being installed/available
+    # locally, therefore adding a condition
+    @pytest.mark.skipif("TRAVIS" in os.environ
+                        and os.environ["TRAVIS"] == "true",
+                        reason="Skipping this test on Travis CI.")
+    def test_scan_no_timeout(self):
+        # This is needed for nmap static library to be added to the path
+        original_pathvar = os.environ['PATH']
+        os.environ['PATH'] = os.path.dirname(os.path.realpath(__file__))  \
+            + '/vendor/dirb2222/' + ':' \
+            + original_pathvar
+        host_name = "infosec.mozilla.org"
+        # By default this will use the short wordlist
+        scanner = DirectoryEnumScanner()
+        return_code, result = scanner.scan(host_name)
+        assert return_code == 0
+        assert 'host' in result
+        assert 'routes' in result
+        assert len(result['routes']) > 0
+        
+        # Set PATH to original value
+        os.environ['PATH'] = original_pathvar
+
+    # Same as the above function
+    @pytest.mark.skipif("TRAVIS" in os.environ
+                        and os.environ["TRAVIS"] == "true",
+                        reason="Skipping this test on Travis CI.")
+    def test_scan_timeout(self):
+        # This is needed for nmap static library to be added to the path
+        original_pathvar = os.environ['PATH']
+        os.environ['PATH'] = os.path.dirname(os.path.realpath(__file__))  \
+            + '/vendor/dirb2222/' + ':' \
+            + original_pathvar
+        host_name = "infosec.mozilla.org"
+        # Give it a long wordlist to guarantee time out
+        scanner = DirectoryEnumScanner(wordlist='long')
+        return_code, result = scanner.scan(host_name)
+        assert not return_code == 0
+        assert not result
